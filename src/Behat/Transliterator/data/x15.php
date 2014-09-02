5d9a6221d54edfed92cb78d4f43269763572c3db
woutersioen <wouter.sioen@wijs.be>
2014-09-02 07:52:31 +0200

    Update more bank-files.
    I update them according to the perl version.
    http://cpansearch.perl.org/src/SBURKE/Text-Unidecode-0.04/lib/Text/Unidecode/

--- src/Behat/Transliterator/data/x15.php
+++ src/Behat/Transliterator/data/x15.php
@@ -1,4 +1,19 @@
 <?php
 $UTF8_TO_ASCII[0x15] = array(
-'[?]','e','aai','i','ii','o','oo','oo','ee','i','a','aa','we','we','wi','wi','wii','wii','wo','wo','woo','woo','woo','wa','wa','waa','waa','waa','ai','w',"'",'t','k','sh','s','n','w','n','[?]','w','c','?','l','en','in','on','an','pe','paai','pi','pii','po','poo','poo','hee','hi','pa','paa','pwe','pwe','pwi','pwi','pwii','pwii','pwo','pwo','pwoo','pwoo','pwa','pwa','pwaa','pwaa','pwaa','p','p','h','te','taai','ti','tii','to','too','too','dee','di','ta','taa','twe','twe','twi','twi','twii','twii','two','two','twoo','twoo','twa','twa','twaa','twaa','twaa','t','tte','tti','tto','tta','ke','kaai','ki','kii','ko','koo','koo','ka','kaa','kwe','kwe','kwi','kwi','kwii','kwii','kwo','kwo','kwoo','kwoo','kwa','kwa','kwaa','kwaa','kwaa','k','kw','keh','kih','koh','kah','ce','caai','ci','cii','co','coo','coo','ca','caa','cwe','cwe','cwi','cwi','cwii','cwii','cwo','cwo','cwoo','cwoo','cwa','cwa','cwaa','cwaa','cwaa','c','th','me','maai','mi','mii','mo','moo','moo','ma','maa','mwe','mwe','mwi','mwi','mwii','mwii','mwo','mwo','mwoo','mwoo','mwa','mwa','mwaa','mwaa','mwaa','m','m','mh','m','m','ne','naai','ni','nii','no','noo','noo','na','naa','nwe','nwe','nwa','nwa','nwaa','nwaa','nwaa','n','ng','nh','le','laai','li','lii','lo','loo','loo','la','laa','lwe','lwe','lwi','lwi','lwii','lwii','lwo','lwo','lwoo','lwoo','lwa','lwa','lwaa','lwaa','l','l','l','se','saai','si','sii','so','soo','soo','sa','saa','swe','swe','swi','swi','swii','swii','swo','swo','swoo','swoo',
+'swa', 'swa', 'swaa', 'swaa', 'swaa', 's', 's', 'sw', 's', 'sk', 'skw', 'sW', 'spwa', 'stwa', 'skwa', 'scwa',
+'she', 'shi', 'shii', 'sho', 'shoo', 'sha', 'shaa', 'shwe', 'shwe', 'shwi', 'shwi', 'shwii', 'shwii', 'shwo', 'shwo', 'shwoo',
+'shwoo', 'shwa', 'shwa', 'shwaa', 'shwaa', 'sh', 'ye', 'yaai', 'yi', 'yii', 'yo', 'yoo', 'yoo', 'ya', 'yaa', 'ywe',
+'ywe', 'ywi', 'ywi', 'ywii', 'ywii', 'ywo', 'ywo', 'ywoo', 'ywoo', 'ywa', 'ywa', 'ywaa', 'ywaa', 'ywaa', 'y', 'y',
+'y', 'yi', 're', 're', 'le', 'raai', 'ri', 'rii', 'ro', 'roo', 'lo', 'ra', 'raa', 'la', 'rwaa', 'rwaa',
+'r', 'r', 'r', 'fe', 'faai', 'fi', 'fii', 'fo', 'foo', 'fa', 'faa', 'fwaa', 'fwaa', 'f', 'the', 'the',
+'thi', 'thi', 'thii', 'thii', 'tho', 'thoo', 'tha', 'thaa', 'thwaa', 'thwaa', 'th', 'tthe', 'tthi', 'ttho', 'ttha', 'tth',
+'tye', 'tyi', 'tyo', 'tya', 'he', 'hi', 'hii', 'ho', 'hoo', 'ha', 'haa', 'h', 'h', 'hk', 'qaai', 'qi',
+'qii', 'qo', 'qoo', 'qa', 'qaa', 'q', 'tlhe', 'tlhi', 'tlho', 'tlha', 're', 'ri', 'ro', 'ra', 'ngaai', 'ngi',
+'ngii', 'ngo', 'ngoo', 'nga', 'ngaa', 'ng', 'nng', 'she', 'shi', 'sho', 'sha', 'the', 'thi', 'tho', 'tha', 'th',
+'lhi', 'lhii', 'lho', 'lhoo', 'lha', 'lhaa', 'lh', 'the', 'thi', 'thii', 'tho', 'thoo', 'tha', 'thaa', 'th', 'b',
+'e', 'i', 'o', 'a', 'we', 'wi', 'wo', 'wa', 'ne', 'ni', 'no', 'na', 'ke', 'ki', 'ko', 'ka',
+'he', 'hi', 'ho', 'ha', 'ghu', 'gho', 'ghe', 'ghee', 'ghi', 'gha', 'ru', 'ro', 're', 'ree', 'ri', 'ra',
+'wu', 'wo', 'we', 'wee', 'wi', 'wa', 'hwu', 'hwo', 'hwe', 'hwee', 'hwi', 'hwa', 'thu', 'tho', 'the', 'thee',
+'thi', 'tha', 'ttu', 'tto', 'tte', 'ttee', 'tti', 'tta', 'pu', 'po', 'pe', 'pee', 'pi', 'pa', 'p', 'gu',
+'go', 'ge', 'gee', 'gi', 'ga', 'khu', 'kho', 'khe', 'khee', 'khi', 'kha', 'kku', 'kko', 'kke', 'kkee', 'kki',
 );
